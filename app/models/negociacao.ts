dd878097287e0f596d09ce8a9f1600c0629f2987
unknown <vieira.henrique02@outlook.com>
2023-03-06 22:33:08 -0300

    finalizando o primeiro curso de ts

---
+++ app/models/negociacao.ts
@@ -0,0 +1,19 @@
+export class Negociacao {
+  constructor(
+    private _data: Date,
+    public readonly quantidade: number,
+    public readonly valor: number,
+    public readonly desc: String
+  ) {}
+
+  
+  get volume(): Number {
+    return this.quantidade * this.valor;
+  }
+
+  get data(): Date{
+    const data = new Date(this._data.getTime())
+    return data
+  }
+}
+
